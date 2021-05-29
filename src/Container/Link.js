14215e4ac7552269f858e7c5c2de803eb6e6978b
dexterous17 <36617353+dexterous17@users.noreply.github.com>
2021-05-29 15:34:15 -0400

    Link updated for the left side

--- src/Container/Link.js
+++ src/Container/Link.js
@@ -4,7 +4,7 @@ export default function Link({data}) {
     const github = "https://github.com/dexterous17?tab=repositories"
     return (
         <div>
-            <a href={github} target={github}>{data.Name}</a>
+            <a href={data.href} target={data.href}>{data.Name}</a>
         </div>
     )
 }
