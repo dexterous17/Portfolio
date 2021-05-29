339bf0309993f58b313a5cf2cc2377affc49a78e
dexterous17 <36617353+dexterous17@users.noreply.github.com>
2021-05-29 17:13:01 -0400

    favicon updated, index.html updated and Link file updated where const github is removed as it is no longer needed

--- src/Container/Link.js
+++ src/Container/Link.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function Link({data}) {
-    const github = "https://github.com/dexterous17?tab=repositories"
     return (
         <div>
             <a href={data.href} target={data.href}>{data.Name}</a>
